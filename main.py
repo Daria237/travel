550e2ae4ba56f5acbae2997181a0e3ee4b4a32f8
Elizav-ta <eaefanova@edu.hse.ru>
2025-10-23 19:15:32 +0300

    2

--- main.py
+++ main.py
@@ -8,3 +8,29 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 # Словари для хранения состояний пользователей
 user_states = {}
+class UrbanExplorer:
+    def __init__(self):
+        self.adventure_data = self._load_adventure_data()
+        self.additional_tasks = self._load_additional_tasks()
+        self.adventure_types = {
+            "1": "photographer",
+            "2": "historian", 
+            "3": "foodie",
+            "4": "fun"
+        }
+        self.districts = {
+            "1": "nizhegorodsky",
+            "2": "sovetsky",
+            "3": "prioksky"
+        }
+        self.district_names = {
+            "nizhegorodsky": "Нижегородский",
+            "sovetsky": "Советский",
+            "prioksky": "Приокский"
+        }
+        self.type_names = {
+            "photographer": "Фотограф-исследователь 📸",
+            "historian": "Историк-детектив 🕵️",
+            "foodie": "Гастроном-путешественник 🍜",
+            "fun": "Фанат атмосферных развлечений 🎭"
+        }
